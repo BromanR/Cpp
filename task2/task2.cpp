9eea773aa2e8e494e16e17488a8a213ff3e5c5cb
BromanR <roma.polozhiev@gmail.com>
2020-09-07 18:14:44 +0300

    Добавьте файлы проекта.

---
+++ task2/task2.cpp
@@ -0,0 +1,22 @@
+﻿// task2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    int n, x;
+
+    std::cout << "Enter amount of numbers\n";
+    scanf_s("%d", &n);
+    scanf_s("%d", &x);
+    int min = x;
+    int max = x;
+    for (int i = 1; i < n; i++) {
+        scanf_s("%d", &x);
+        if (x > max) { max = x; }
+        if (x < min) { min = x; }
+    }
+    printf("maximum = %d\n", max);
+    printf("minimum = %d", min);
+}
